a1c64ef84935e40d0972a919168f6f05f1f4aeae
John Mettraux <jmettraux@gmail.com>
2021-02-06 19:37:57 +0900

    Add halt helper function

--- lib/cigarillo.rb
+++ lib/cigarillo.rb
@@ -1,5 +1,25 @@
 
 require 'uri'
+require 'webrick'
 
 PARAMS = URI.decode_www_form(ENV['QUERY_STRING'])
+
+def halt(code, message=nil, &block)
+
+  message =
+    case message
+    when String then message
+    when nil then (WEBrick::HTTPStatus.reason_phrase(code) rescue nil)
+    else nil
+    end
+  message ||=
+    "Unknown code #{code.inspect}"
+
+  puts "Status: #{code} #{message}"
+  puts "Content-Type: text/plain"
+  puts
+  puts block ? block.call : ''
+
+  exit 0
+end
 
